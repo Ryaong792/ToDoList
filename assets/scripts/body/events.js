372bc8bac5e14f51a81be73fee463a05631ae734
ryaong792 <ryan.ongchoa@libertymutual.com>
2017-04-25 19:02:31 -0400

    Added login, nav bar, background images etc
    Added:
    file: /body/events.js.  JS file for main body for the page
    file: color.scss - host all color names for SCSS
    file: /body/images/background.png  background image for main page
    file: /body/images/grey.png  backup image file for main page
    file: login.scss CSS file for login and wrapper for login
    file: typography.scss main scss for main body for the page
    
    Changes:
    index.js - connected the body/events.js and addhandlers
    index.scss - tied all new SCSS files
    index.html - updated main HTML with the following:
              - animate.css link
              - material design links
              - nav bar
              - login wrapper and login prompts
    
    NOTES:  this is the 2nd commit, no bugs was found.  Login
    works without API and all links and JS file are working normally.
    Will need to setup a logout feature and TASK list to add/remove etc.

---
+++ assets/scripts/body/events.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
+
+// **** LOGIN BUTTONS / WECLOME WINDOW! ****
+
+// ****login****
+$('#login-button').click(function (event) {
+  event.preventDefault()
+  $('form').fadeOut(500)
+  $('.nav').show()
+  // need to remove this class when signout happens!
+  $('.wrapper').addClass('form-success animated rotateOut')
+})
+
+// ****register-form ****
+$('#register-button').click(function (event) {
+  event.preventDefault()
+  $('form').fadeOut(500)
+// This login will be removed - Will add to API area instead
+  $('.login-form').show()//
+})
+
+$('.register').click(function () {
+  event.preventDefault()
+  $('.login-form').hide()
+  $('.register-form').show()
+})
+
+$('.sign-in').click(function (event) {
+  event.preventDefault()
+  $('form').fadeOut(500)
+// This login will be removed - Will add to API area instead
+  $('.login-form').show()//
+})
+
+// ************** end of login *************************
+// **** start of adding task and removing task ****
+
+
+
+
+const addHandlers = () => {
+  $('.register-form').hide()
+  $('.nav').hide()
+}
+
+module.exports = {
+  addHandlers
+}
