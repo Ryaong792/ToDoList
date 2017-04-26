96772594c966500bf85f2ba26137591d6448f126
ryaong792 <ryan.ongchoa@libertymutual.com>
2017-04-25 23:49:09 -0400

    Added logout button Added: Logout button - index.html added logout button typography - added font size to make icon a little bigger Events.js  - added out functions to make login wrapper appear and              remove added class and added class on signin/out for              animated affects.

--- assets/scripts/body/events.js
+++ assets/scripts/body/events.js
@@ -11,6 +11,8 @@ $('#login-button').click(function (event) {
   $('.nav').show()
   // need to remove this class when signout happens!
   $('.wrapper').addClass('form-success animated rotateOut')
+  $('.logout').removeClass('animated fadeOutUp')
+  $('.logout').show().addClass('animated fadeInDown')
 })
 
 // ****register-form ****
@@ -31,18 +33,25 @@ $('.sign-in').click(function (event) {
   event.preventDefault()
   $('form').fadeOut(500)
 // This login will be removed - Will add to API area instead
-  $('.login-form').show()//
+  $('.login-form').show()
+})
+
+$('.logout').click(function (event) {
+  event.preventDefault()
+  $('form').fadeIn(500)
+  $('.nav').hide()
+  $('.logout').removeClass('animated fadeInDown')
+  $('.logout').addClass('animated fadeOutUp')
+  $('.wrapper').fadeIn(700)
+  $('.wrapper').removeClass('form-success animated rotateOut')
 })
 
 // ************** end of login *************************
 // **** start of adding task and removing task ****
-
-
-
-
 const addHandlers = () => {
   $('.register-form').hide()
   $('.nav').hide()
+  $('.logout').hide()
 }
 
 module.exports = {
